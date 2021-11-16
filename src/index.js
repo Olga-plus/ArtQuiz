378bf2048a67f78aa650ddc7935e3c4c65e5380f
Olga <klokova.plus@gmail.com>
2021-11-16 14:13:00 +0100

    fix: webpack config

--- src/index.js
+++ src/index.js
@@ -1,22 +1,7 @@
-import example from './img/118.jpg'
+
 import './styles/main.scss';
-import './views/page';
+import {render} from './views/page';
 
 console.log("Как интересно!")
 
-class Game {
-    name = 'Violin Charades'
-}
-const myGame = new Game()
-
-// создаем параграф
-const p = document.createElement('p')
-p.textContent = `I like ${myGame.game}.`
-
-// создаем элемент заголовка
-const heading = document.createElement('h1')
-heading.textContent = 'Как интересно!'
-
-// добавляем параграф и заголовок в DOM
-const root = document.querySelector('#root')
-root.append(heading, p)
+render();
